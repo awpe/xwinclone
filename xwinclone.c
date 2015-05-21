bb6ae3fe24d927b6cfd0d24277ec8b6ef521a9af
awpe <awpe00@gmail.com>
2015-05-21 12:57:50 +0200

    -redundant variables were deleted -some code moved to xwclib -program flow changed

--- xwinclone.c
+++ xwinclone.c
@@ -8,38 +8,15 @@ main (int     argc,
     Display              * xDpy;
     Screen               * xScr;
     Window                 srcWin, rootWin , trgWin;
-    XColor                 trgWinBgClr;
-    Colormap               xClrMap;
     XWindowAttributes      srcWinAttr, trgWinAttr, rootWinAttr;
     XSetWindowAttributes   trgWinSetAttr;
-    int                    srcWinWidth, srcWinHeight, srcWinDepth, trgWinWidth,
-        trgWinHeight, rootWinHeight, rootWinWidth;
     GC                     xGraphicsCtx;
-    //XEvent                 xEvent;
     XVisualInfo            xVisInfo;
     Visual               * xVis;
     Pixmap                 pm;
-    struct timespec        focusDelay, frameDelay;
-    /*
-        int                 srcWinWidth, srcWinHeight, trgWinWidth, trgWinHeight,
-            widthDelta, heightDelta, ptrMode, kbdMode, keycode, x11_fd;
-        unsigned int        modifiers;
-        Bool                ownerEvents;
-        XEvent              xEvent;
-        XImage            * xImage, * xImageTmp;
-        fd_set              in_fds;
-        char                shouldQuit;
-     */
-
-
-    if (( prgCfg = processArgs (argc, (const char **) argv) ) == NULL)
-    {
-        return EXIT_FAILURE;
-    }
-
+    
     if (( xDpy = openDefaultDisplay () )  == NULL)
     {
-        free (prgCfg);
         return EXIT_FAILURE;
     }
 
@@ -48,27 +25,17 @@ main (int     argc,
     if (chkCompExt (xDpy) == False)
     {
         XCloseDisplay (xDpy);
-        free (prgCfg);
         return EXIT_FAILURE;
     }
 
-    focusDelay.tv_nsec = 0;
-    focusDelay.tv_sec  = prgCfg->focusTime;
-
-    frameDelay.tv_nsec = ( 1.0 / prgCfg->frameRate ) * 1000000000L;
-    frameDelay.tv_sec  = 0;
-
-    printf ("Selected focustime is %d\n"
-            "Selected framerate is %d, this results in %ldns frame delay\n"
-            "Selected autocentering mode is %d(bool)\n"
-            "Selected top offset is %d(px)\n"
-            "Selected bgcolor is %s(#rrggbb)\n\n",
-            prgCfg->focusTime, prgCfg->frameRate,
-            frameDelay.tv_nsec, prgCfg->autoCenter,
-            prgCfg->topOffset, prgCfg->bgColor);
+    if (( prgCfg = processArgs (xDpy, argc, (const char **) argv) ) == NULL)
+    {
+        XCloseDisplay (xDpy);
+        return EXIT_FAILURE;
+    }
 
     printf ("Now move focus to window you want to be cloned...\n");
-    nanosleep (&focusDelay, NULL);
+    nanosleep (&prgCfg->focusDelay, NULL);
 
     if (( srcWin = getActiveWindow (xDpy) ) == None)
     {
@@ -88,10 +55,6 @@ main (int     argc,
 
     printWindowInfo (xDpy, srcWin, &srcWinAttr);
 
-    srcWinHeight = srcWinAttr.height;
-    srcWinWidth  = srcWinAttr.width;
-    srcWinDepth  = srcWinAttr.depth;
-
     if (( xScr = getScreenByWindowAttr (xDpy, &srcWinAttr) ) == None)
     {
         XCloseDisplay (xDpy);
@@ -99,7 +62,8 @@ main (int     argc,
         return EXIT_FAILURE;
     }
 
-    if (( rootWin = getScrRootWin (xScr) ) == None)
+
+    if (( rootWin = getRootWinOfScr (xScr) ) == None)
     {
         XCloseDisplay (xDpy);
         free (prgCfg);
@@ -115,44 +79,17 @@ main (int     argc,
         return EXIT_FAILURE;
     }
 
-    rootWinWidth  = rootWinAttr.width;
-    rootWinHeight = rootWinAttr.height;
-
-    if (grabExitKey (xDpy, rootWin) == False)
+    if (grabExitKey (xDpy, rootWin, prgCfg) == False)
     {
         XCloseDisplay (xDpy);
         free (prgCfg);
         return EXIT_FAILURE;
     }
 
-    memset (&trgWinBgClr, 0, sizeof (trgWinBgClr ));
-    xClrMap = DefaultColormapOfScreen (xScr);
-
-    /*
-     * Maybe this color string parsing must take place 
-     * while processing options...
-     */
-    printf ("\nParsing the window background color string \"%s\" ... ",
-            prgCfg->bgColor);
-
-    if (!XParseColor (xDpy, xClrMap, prgCfg->bgColor, &trgWinBgClr)
-        || !XAllocColor (xDpy, xClrMap, &trgWinBgClr))
-    {
-        printf ("Error:\n\tXParseColor and/or XAllocColor error\n");
-        XCloseDisplay (xDpy);
-        free (prgCfg);
-        return EXIT_FAILURE;
-    }
-    printf ("Success\n");
-
-    /* redundant information
-    printf ("Color parsing result: pixel=%ld, red=%d, green=%d, blue=%d\n",
-            xColor.pixel, xColor.red, xColor.green, xColor.blue);
-     */
     printf ("Creating translation window ... ");
 
-    if (!XMatchVisualInfo (xDpy, XScreenNumberOfScreen (xScr), srcWinDepth,
-                           TrueColor, &xVisInfo))
+    if (! XMatchVisualInfo (xDpy, XScreenNumberOfScreen (xScr),
+                            srcWinAttr.depth, TrueColor, &xVisInfo))
     {
         printf ("Error: no such visual\n");
         XCloseDisplay (xDpy);
@@ -172,14 +109,14 @@ main (int     argc,
     trgWinSetAttr.colormap         = XCreateColormap (xDpy,
                                                       XDefaultRootWindow (xDpy),
                                                       xVis, AllocNone);
-    trgWinSetAttr.background_pixel = trgWinBgClr.pixel;
+    trgWinSetAttr.background_pixel = prgCfg->bgColor.pixel;
     trgWinSetAttr.border_pixel     = 0;
     trgWinSetAttr.bit_gravity      = NorthWestGravity;
 
-    trgWin = XCreateWindow (xDpy, rootWin, 0, 0, srcWinWidth, srcWinHeight, 0,
-                            srcWinDepth, InputOutput, xVis, CWBackPixel |
-                            CWColormap | CWBorderPixel | CWBitGravity,
-                            &trgWinSetAttr);
+    trgWin = XCreateWindow (xDpy, rootWin, 0, 0, srcWinAttr.width,
+                            srcWinAttr.height, 0, srcWinAttr.depth,
+                            InputOutput, xVis, CWBackPixel | CWColormap
+                            | CWBorderPixel | CWBitGravity, &trgWinSetAttr);
 
     if (getXErrState () == True)
     {
@@ -239,12 +176,12 @@ main (int     argc,
 
     printWindowInfo (xDpy, trgWin, &trgWinAttr);
 
-    pm = XCreatePixmap (xDpy, rootWin, rootWinWidth, rootWinHeight,
-                        srcWinDepth);
+    pm = XCreatePixmap (xDpy, rootWin, rootWinAttr.width, rootWinAttr.height,
+                        srcWinAttr.depth);
     //Pixmap pmDB = XCreatePixmap (xDisp, rootWin, 1920, 1200, srcWinDepth);
     xGraphicsCtx = XCreateGC (xDpy, pm, 0, NULL);
 
-    XSetForeground (xDpy, xGraphicsCtx, trgWinBgClr.pixel);
+    XSetForeground (xDpy, xGraphicsCtx, prgCfg->bgColor.pixel);
     //XSetBackground (xDisp, xGraphicsCtx, trgWinBgClr.pixel);
     //XSetFillStyle (xDisp, xGraphicsCtx, FillSolid);
 
@@ -255,12 +192,12 @@ main (int     argc,
 
     while (1)
     {
-/*
-        while (XPending (xDpy))
-        {
-            XNextEvent (xDpy, &xEvent);
-        }
-*/
+        /*
+                while (XPending (xDpy))
+                {
+                    XNextEvent (xDpy, &xEvent);
+                }
+         */
 
         XGetWindowAttributes (xDpy, srcWin, &srcWinAttr);
 
@@ -273,9 +210,6 @@ main (int     argc,
             return EXIT_FAILURE;
         }
 
-        srcWinHeight = srcWinAttr.height;
-        srcWinWidth  = srcWinAttr.width;
-
         XGetWindowAttributes (xDpy, trgWin, &trgWinAttr);
 
         if (getXErrState () == True)
@@ -287,26 +221,23 @@ main (int     argc,
             return EXIT_FAILURE;
         }
 
-        trgWinHeight = trgWinAttr.height;
-        trgWinWidth  = trgWinAttr.width;
-
-        XFillRectangle (xDpy, pm, xGraphicsCtx, 0, 0, trgWinWidth,
-                        trgWinHeight);
+        XFillRectangle (xDpy, pm, xGraphicsCtx, 0, 0, trgWinAttr.width,
+                        trgWinAttr.height);
 
         pm2 = XCompositeNameWindowPixmap (xDpy, srcWin);
 
-        XCopyArea (xDpy, pm2, pm, xGraphicsCtx, 0, 0, trgWinWidth,
-                   trgWinHeight, 0, 0);
+        XCopyArea (xDpy, pm2, pm, xGraphicsCtx, 0, 0, trgWinAttr.width,
+                   trgWinAttr.height, 0, 0);
 
-/*
-        XCopyArea (xDpy, srcWin, pm, xGraphicsCtx, 0, 0, trgWinWidth,
-                   trgWinHeight, 0, 0);
-*/
+        /*
+                XCopyArea (xDpy, srcWin, pm, xGraphicsCtx, 0, 0, trgWinWidth,
+                           trgWinHeight, 0, 0);
+         */
 
-        XCopyArea (xDpy, pm, trgWin, xGraphicsCtx, 0, 0, trgWinWidth,
-                   trgWinHeight, 0, 0);
+        XCopyArea (xDpy, pm, trgWin, xGraphicsCtx, 0, 0, trgWinAttr.width,
+                   trgWinAttr.height, 0, 0);
 
-        nanosleep (&frameDelay, NULL);
+        nanosleep (&prgCfg->frameDelay, NULL);
     }
     XFreePixmap (xDpy, pm);
     XDestroyWindow (xDpy, trgWin);
