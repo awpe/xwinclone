b859344a52b79d3b578ef9057196852a7b2b2cdd
Anton Lantcevich <awpe00@gmail.com>
2015-06-03 01:48:15 +0200

    Version 0.1a -version information -default fps changed

--- defines.h
+++ defines.h
@@ -4,14 +4,25 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
+
+#define XWINCLONE_VERSION_STR                        "0.1a"
+
+    /**************************************************************************/
+    /*Program definitions*/
+#define STR_EQUAL                                    0
+#define INT_CONV_OK                                  0
+#define LOG_LVL_NO                                   0
+#define LOG_LVL_1                                    1
+#define LOG_LVL_2                                    2
+    /**************************************************************************/
+
     /**************************************************************************/
     /*Options*/
 #define _POSIX_C_SOURCE                              200112L
-#define FRAMERATE_FPS                                2  //fps
+#define FRAMERATE_FPS                                30  //fps
 #define TIME_TO_CHANGE_FOCUS_SEC                     2   //s
 #define TIME_TO_WAIT_FOR_SRCWIN_TO_BECOME_VISIBLE_MS 500 //ms
 #define DEFAULT_BG_COLOR                             "000000" //rgb color
-    //#define EXIT_KEY                                     XK_g // from keysymdef.h
 #define EXIT_KEY_STR                                 "g"
 #define EXIT_MASK                                    ControlMask // from X.h
 #define DEF_EXIT_KOMBINATION_STR                     "CTRL + g"
@@ -21,16 +32,7 @@ extern "C" {
 #define TOP_OFFSET                                   71 // in pixels
 #define PROGRAM_EXE_NAME_STR                         "xwinclone"
 #define BG_IMAGE_PATH                                "./"
-#define DEFAULT_LOG_LVL                              0
-    /**************************************************************************/
-
-    /**************************************************************************/
-    /*Program definitions*/
-#define STR_EQUAL                                    0
-#define INT_CONV_OK                                  0
-#define LOG_LVL_NO                                   0
-#define LOG_LVL_1                                    1
-#define LOG_LVL_2                                    2
+#define DEFAULT_LOG_LVL                              LOG_LVL_NO
     /**************************************************************************/
 #ifdef	__cplusplus
 }
